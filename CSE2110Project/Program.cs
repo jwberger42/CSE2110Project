dac2ed7cbe832145e52a56380fdf8ca190d877d2
John Berger <jwberger@shaw.ca>
2017-03-14 09:21:10 -0600

    Added user deteciton and rng

--- CSE2110Project/Program.cs
+++ CSE2110Project/Program.cs
@@ -8,9 +8,39 @@ namespace CSE2110Project
 {
     class Program
     {
+        static String input;
+        static int choice;
+        static int cpuChoice;
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a test of C#!");
+            Console.WriteLine("Rock, paper, scissors, lizard, or Spock?!");
+            input = Console.ReadLine().ToLower();
+            switch (input)
+            {
+                case "rock":
+                    Logic(0);
+                    break;
+                case "paper":
+                    Logic(1);
+                    break;
+                case "scissors":
+                    Logic(2);
+                    break;
+                case "lizard":
+                    Logic(3);
+                    break;
+                case "spock":
+                    Logic(4);
+                    break;
+                default:
+                    Console.WriteLine("Don't enter something that is not a choice!!");
+                    break;
+            }
+        }
+        static void Logic(int userin)
+        {
+            Random rand = new Random();
+            cpuChoice = rand.Next(0,5);
         }
     }
 }
